432d2313916488fef92f79f2b933b49eef33f392
Fuad B Sahmawi <fuadsahmawi@gmail.com>
2018-02-14 21:03:02 +0800

    Parser.java: Added new argument format for edit command

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -32,6 +32,8 @@ public class Parser {
 
     public static final Pattern PERSON_INDEX_ARGS_FORMAT = Pattern.compile("(?<targetIndex>.+)");
 
+    public static final Pattern PERSON_NAME_EDIT_ARGS_FORMAT = Pattern.compile ("(?<targetIndex>.+)" + " (?<name>[^/]+)");
+
     public static final Pattern KEYWORDS_ARGS_FORMAT =
             Pattern.compile("(?<keywords>\\S+(?:\\s+\\S+)*)"); // one or more keywords separated by whitespace
 
