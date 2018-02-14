1930ebd6884475c43e592b9a8d1e229ed3485a3d
Fuad B Sahmawi <fuadsahmawi@gmail.com>
2018-02-14 23:14:58 +0800

    EditCommand.java: 1. Added comments for this class 2. Edited the message_usage string 3. Added new attributes for this class 4. Edited the execute() function

--- src/seedu/addressbook/commands/EditCommand.java
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -1,31 +1,41 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
+
+/**
+ * Edits a person's name identified using it's last displayed index from the address book.
+ */
 
 public class EditCommand extends Command {
     public static final String COMMAND_WORD = "edit";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the last person listing.\n"
+            + ": Edits the person's name identified by the index number used in the last person listing.\n"
             + "Parameters: INDEX\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Example: " + COMMAND_WORD + " 1" + " Susan";
 
-    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
+
+    private final int indexToEdit;
+    private final String nameToEdit;
 
 
-    public EditCommand(int targetVisibleIndex) {
-        super(targetVisibleIndex);
+    public EditCommand(int index, String name) {
+        this.indexToEdit = index;
+        this.nameToEdit = name;
     }
 
 
     @Override
     public CommandResult execute() {
         try {
+            setTargetIndex(this.indexToEdit);
             final ReadOnlyPerson target = getTargetPerson();
-            addressBook.removePerson(target);
-            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, target));
+            addressBook.editName(this.indexToEdit, this.nameToEdit);
+            return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, target));
 
         } catch (IndexOutOfBoundsException ie) {
             return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
