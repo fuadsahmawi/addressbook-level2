88dc1609496afdf80dae9ebca574c0090d28710b
Fuad B Sahmawi <fuadsahmawi@gmail.com>
2018-02-14 23:30:55 +0800

    SortCommand.java: Added comments for sorting command

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -6,6 +6,10 @@ import seedu.addressbook.data.person.ReadOnlyPerson;
 import java.util.Comparator;
 import java.util.List;
 
+
+/**
+ * Sorts the list of all persons in the address book by name in alphabetical order.
+ */
 public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
